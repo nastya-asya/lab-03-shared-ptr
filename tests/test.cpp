604da9cd72f71d63f57e757d463ab9f977f09b91
nastya-asya <61165064+nastya-asya@users.noreply.github.com>
2021-01-13 18:37:09 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,54 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Anastasiya Smirnova nastya.asya08@yandex.ru
 
 #include <gtest/gtest.h>
+#include "header.hpp"
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+TEST(Example, EmptyTest) { EXPECT_TRUE(true); }
+
+TEST(SelfSharedPtr, func1) {
+  int k = 1000;
+  SharedPtr<int> test0(&k);
+  ASSERT_EQ(*test0, k);
 }
+
+TEST(SharedPtr, funcOperator) {
+  int k = 1000;
+  SharedPtr<int> test0(&k);
+  SharedPtr<int> test1 = test0;
+  ASSERT_EQ(*test1, k);
+}
+
+TEST(SharedPtr, funcCountOfLinks) {
+  int k = 1000;
+  SharedPtr<int> test0(&k);
+  SharedPtr<int> test1(test0);
+  SharedPtr<int> test2 = test1;
+  ASSERT_EQ(test0.useCount(), 3);
+}
+
+TEST(SharedPtr, funcSwap) {
+  int a = 1;
+  int b = 2;
+  SharedPtr<int> A(&a);
+  SharedPtr<int> B(&b);
+  B.swap(A);
+  ASSERT_EQ(*B, 1);
+}
+
+TEST(SharedPtr, funcRvalueOperator) {
+  int a = 1;
+  SharedPtr<int> A(&a);
+  SharedPtr<int> B(std::move(A));
+  ASSERT_EQ(B.useCount(), 2);
+}
+
+TEST(SharedPtr, funcBool) {
+  SharedPtr<int> A;
+  ASSERT_EQ(A, false);
+}
+
+TEST(SharedPtr, funcThrow) {
+  SharedPtr<int> A;
+  ASSERT_THROW(*A, std::out_of_range);
+}
+
